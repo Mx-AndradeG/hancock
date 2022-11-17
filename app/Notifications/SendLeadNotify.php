b1ea3a6b9fb9cb04f3a03752d166b800367ed851
Mx-AndradeApplab <landrade@applab.mx>
2022-11-17 17:28:42 -0600

    feat: :sparkles: Se agrego la notificacion de lead

---
+++ app/Notifications/SendLeadNotify.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class SendLeadNotify extends Notification
+{
+    use Queueable;
+    public $lead;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($lead)
+    {
+        $this->lead = $lead;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                ->line('Un nuevo lead quiere informacion !')
+                ->line('Estos son sus datos')
+                ->line('Nombre: '. $this->lead->name)
+                ->line('Telefono: '. $this->lead->phone)
+                ->line('Correo: '. $this->lead->email)
+                ->line('Mensaje: '. $this->lead->message);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name'          => 'Soporte',
+            'email'         => 'ventas@hancock.com.mx',
+            'email_verified_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'password'      => Hash::make('123456789'),
+            'created_at'    => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at'    => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
